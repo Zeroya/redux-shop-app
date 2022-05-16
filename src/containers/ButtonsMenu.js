fac0e85407dfad7d2fb287019ab5ce0c008d7c70
Yan Latyshev <yan.latyshev@gmail.com>
2022-05-16 19:21:33 +0300

    added sort button

--- src/containers/ButtonsMenu.js
+++ src/containers/ButtonsMenu.js
@@ -1,24 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { sortProducts } from '../redux/actions/productsActions';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const ButtonsMenu = () => {
   const category = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const options = [
+    { value: 'Sort', label: 'Sort' },
+    { value: '1', label: 'Start' },
+    { value: '2', label: 'End' },
+  ];
+
+  const [selectedOption, setSelectedOption] = useState({});
+  console.log(selectedOption);
+
+  const onChanged = (e) => {
+    navigate('/' + e.target.value);
+  }
 
   return (
     <div style={{ marginTop: `${category.categoryId === undefined ? "5.5em" : "0.5em"}`, display: "flex", justifyContent: 'center' }} >
 
-
-      <select style={{ marginRight: "1em", }} class="ui inverted secondary dropdown">
+      <select onChange={onChanged} style={{ marginRight: "1em", }} class="ui inverted secondary dropdown">
         <option value="">Sort</option>
-        <option  value="1">Start</option>
-        <option  value="0"><Link to={'/desc'}>End</Link></option>
+        <option value="">Start</option>
+        <option value="desc">End</option>
       </select>
 
-      <Link to={`/desc`}>
+      <Link to={`/`}>
         <button class="ui inverted secondary button">
           All
         </button>
